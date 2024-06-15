247793221ffdea04208bb052d00ddb750ad37224
andreaoliveira9 <andreaoliveira@ua.pt>
2024-06-15 16:04:48 +0100

    trailblazer

--- info.js
+++ info.js
@@ -143,47 +143,67 @@ let info = {
   ],
   portfolio: [
     {
-      name: "Sheet Recognizer",
+      name: "TrailBlazer",
       pictures: [
         {
-          img: require("./src/assets/portfolio/msr/one.png"),
+          img: require("./src/assets/portfolio/trailblazer/LogoBlack.png"),
         },
         {
-          img: require("./src/assets/portfolio/msr/two.png"),
+          img: require("./src/assets/portfolio/trailblazer/RecommendationAssessment.png"),
         },
         {
-          img: require("./src/assets/portfolio/msr/three.png"),
+          img: require("./src/assets/portfolio/trailblazer/HomePage.png"),
         },
         {
-          img: require("./src/assets/portfolio/msr/four.png"),
+          img: require("./src/assets/portfolio/trailblazer/OfferList.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/trailblazer/BasePage.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/trailblazer/Payment.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/trailblazer/Review.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/trailblazer/MyOffers.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/trailblazer/MyTransactions.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/trailblazer/Dashboard.png"),
+        },
+        {
+          img: require("./src/assets/portfolio/trailblazer/Analysis.png"),
         },
       ],
-      technologies: ["Computer Vision", "OpenCV", "AI", "Flask", "Sockets"],
-      category: "Algorithm",
-      date: "Jan, 2020 - Mar, 2020",
-      github:
-        "https://github.com/hrishikeshpaul/music-sheet-recognizer-backend",
-      visit: "https://github.com/hrishikeshpaul/music-sheet-recognizer-backend",
+      technologies: [
+        "TypeCript",
+        "PostgreSQL",
+        "MongoDB",
+        "React",
+        "RabbitMQ",
+        "Docker",
+        "Firebase",
+        "fastAPI",
+        "NGINX",
+        "Elasitcsearch",
+        "Kibana",
+      ],
+      category: "Website",
+      date: "Feb, 2024 - Jun, 2024",
+      github: "https://github.com/PI-Trailblazer/deployement",
+      visit: "https://pi-trailblazer.github.io/microsite/",
       description:
-        "Music Sheet Recognizer (MSR) highlights basic Image Processing and Recognition. Like Optical Character Recognition, this tool is used to detect music notes and rests. It makes use of basic Computer Vision techniques such as Convolution, Edge Detections, Hough Transforms and Template Matching to detect notes and rests on a music sheet. ",
+        "Trailblazer is a groundbreaking initiative aimed at revitalizing the tourism industry through the integration of Machine Learning (ML) and Natural Language Processing (NLP) technologies. By developing an advanced Destination Management System (DMS), Trailblazer seeks to empower Destination Management Organizations (DMOs) with predictive analytics to foresee trends and make informed decisions in infrastructure, marketing, and sustainability practices. The system analyzes tourists' feedback across various platforms to derive insights into preferences and sentiments, enhancing service quality and personalizing travel experiences. At its core, Trailblazer features a personalized recommendation engine that curates activities tailored to individual interests, fostering a seamless exploration of new experiences while enabling providers to effectively showcase their offerings. This multifaceted approach benefits tourists with customized travel suggestions, provides attraction providers with valuable market insights, and equips DMOs with sophisticated analytical tools for sustainable tourism development.",
     },
     {
       name: "NoQ Job Portal",
       pictures: [
         {
-          img: require("./src/assets/portfolio/noq/logo.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/noq/one.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/noq/two.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/noq/three.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/noq/four.png"),
+          img: require("./src/assets/portfolio/trailblazer/LogoBlack.png"),
         },
       ],
       technologies: ["Node", "Vue", "Express", "MongoDB", "Heroku"],
@@ -198,10 +218,7 @@ let info = {
       name: "POST",
       pictures: [
         {
-          img: require("./src/assets/portfolio/post/one.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/post/two.png"),
+          img: require("./src/assets/portfolio/trailblazer/RecommendationAssessment.png"),
         },
       ],
       technologies: [
@@ -217,192 +234,6 @@ let info = {
       visit: "https://post-client.herokuapp.com/",
       description:
         "Post is a simple algorithm that was developed to tagging a word corresponding to its part of speech. The algorithm makes of a probabilisitic approach along with some randomness, together which forms the basis of an algorithm called Gibbs Sampling.",
-    },
-    {
-      name: "Suicide Analyzer",
-      pictures: [
-        {
-          img: require("./src/assets/portfolio/suicide/suicide-analyzer.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/suicide/suicide2-original.png"),
-        },
-      ],
-      technologies: ["Node", "Angular", "Express", "Leaflet", "Pandas"],
-      category: "Website",
-      date: "Oct, 2018 - Nov, 2018",
-      github: "https://github.com/hrishikeshpaul/whodata",
-      visit: "https://skylarktiral.herokuapp.com/",
-      description:
-        "This is a web application, uses a map to demonstrate the number suicides committed around the globe, according to a dataset provided by the WHO. The application consists of a Leaflet map that colors every nation, based on the number of suicides that have been committed in that country, for that year. It provides us with the capability to select the data as well as the continent.",
-    },
-    {
-      name: "Sentiment Analyzer",
-      pictures: [
-        {
-          img: require("./src/assets/portfolio/smit/one.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/smit/two.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/smit/three.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/smit/four.png"),
-        },
-      ],
-      technologies: ["LSTM", "Keras", "Angular", "Tweepy", "Heroku", "Python"],
-      category: "Alogrithm",
-      date: "Feb, 2018 - Apr  , 2018",
-      github: "https://github.com/hrishikeshpaul/sih",
-      visit: "https://angel.co/projects/1009777-sentiment-analysis",
-      description:
-        "Developing this for the final round of Smart India Hackathon 2018, the web application is a platform for showcasing the sentiment of the tweets of the people in the Telangana region in India on policy satisfaction, services and problem definitions. The minimal UI design consists of different bins, that have their respective tweets, all scraped from twitter. There is also a simple graphical representation denoting the sentiment of the tweets.",
-    },
-    {
-      name: "Clumpr",
-      pictures: [
-        {
-          img: require("./src/assets/portfolio/clumpr/logo.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/clumpr/one.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/clumpr/two.png"),
-        },
-      ],
-      technologies: ["Node", "Angular", "Express", "MySQL", "Facebook API"],
-      category: "Web App",
-      github: "https://github.com/hrishikeshpaul/clumpr",
-      date: "Sep, 2017 - Nov, 2017",
-      visit: "https://github.com/hrishikeshpaul/clumpr",
-      description:
-        'Clumpr is an online social event planning assistant that - using information about similar events happening in similar locations around the same time, friends who frequently attend the same events, and mutual friends - helps users to collaborate, combine, and attend events together, effectively "clumping" people and activities in a way that makes intuitive sense.',
-    },
-    {
-      name: "Nutricare",
-      pictures: [
-        {
-          img: require("./src/assets/portfolio/nc/one.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/nc/two.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/nc/three.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/nc/four.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/nc/five.png"),
-        },
-        {
-          img: require("./src/assets/portfolio/nc/six.png"),
-        },
-      ],
-      technologies: ["Node", "Angular", "Express", "MongDB", "Medical"],
-      category: "Website",
-      github: "https://github.com/hrishikeshpaul/clumpr",
-      date: "May, 2017 - Aug, 2017",
-      visit: "https://angel.co/projects/576300-nutricare",
-      description:
-        "Nutricare is a basic health website for users(patients) to input nutrient intake data and researchers to analyse the data with the help of graphs and tables. A researcher can conduct studies on N different subjects (patient) for a particular problem statement. Typically, a researcher will enter food item consumed by subjects over a period of time (Ex - 24hrs, or 1 week, or 1 month etc).",
-    },
-  ],
-  portfolio_design: [
-    {
-      name: "Guituna",
-      title: "Guituna - Mockup Design",
-      pictures: [
-        {
-          img: require("./src/assets/designs/coursera1/MoodBoard.png"),
-          title: "MoodBoard",
-        },
-        {
-          img: require("./src/assets/designs/coursera1/Mockups 1.png"),
-          title: "Mockups 1",
-        },
-        {
-          img: require("./src/assets/designs/coursera1/Mockups 2.png"),
-          title: "Mockups 2",
-        },
-        {
-          img: require("./src/assets/designs/coursera1/App Elements.png"),
-          title: "App Elements",
-        },
-      ],
-      technologies: ["XD", "Illustrator"],
-      category: "Visual Design",
-      github: "",
-      date: "May, 2020 - Jun, 2020",
-      visit: "",
-      description:
-        "Guituna is a simple, lightweight and intuitive guitar tuner, that provides different modes of guitars based on your usage. You can either select a specific string to tune, or free tune your guitar based on the frequency shown on the meter. Guituna also provides a handful of different tunings to help explore various tuning paradigms. There is also an array of settings that can be configured to your liking based on the devices you're using. <br/><br/>The interface aims to highlight the use of a minimal design and providing just enough controls to make for a great tuner, while exposing various possibilities of guitarists to explore.",
-    },
-    {
-      name: "Pantree",
-      title: "Pantree - Mockup Design",
-      pictures: [
-        {
-          img: require("./src/assets/designs/pantree/MoodBoard.png"),
-          title: "Moodboard",
-        },
-        {
-          img: require("./src/assets/designs/pantree/1.png"),
-          title: "Mockups 1",
-        },
-        {
-          img: require("./src/assets/designs/pantree/2.png"),
-          title: "Mockups 2",
-        },
-      ],
-      technologies: ["XD", "Illustrator"],
-      category: "Visual Design",
-      github: "",
-      date: "May, 2020 - July, 2020",
-      visit: "",
-      description:
-        "Pantree is a recipiece finding and recommending application. It is often the case that you have run to your closest grocery store just to pick up one item that is missing in your kitchen. Pantree keeps a track of all your ingredients, and suggests recipes based on your meal preferences and past recipes. <br/><br/> The goal of the interface is to keep it similar to various recipe/food apps to reduce the learning required by the user, but also provide a smooth interface for users to seemlessly browse through recipes and ingredients.",
-    },
-    {
-      name: "Bunder",
-      title: "Bunder - MVP Proposal",
-      pictures: [
-        {
-          img: require("./src/assets/designs/bunder/Moodboard.png"),
-          title: "Moodboard",
-        },
-        {
-          img: require("./src/assets/designs/bunder/Mockup 1.png"),
-          title: "Mockup 1",
-        },
-        {
-          img: require("./src/assets/designs/bunder/Mockup 2.png"),
-          title: "Mockup 2",
-        },
-        {
-          img: require("./src/assets/designs/bunder/Mockup 3.png"),
-          title: "Mockup 3",
-        },
-        {
-          img: require("./src/assets/designs/bunder/Market Research 1.png"),
-          title: "Market Research 1",
-        },
-        {
-          img: require("./src/assets/designs/bunder/Market Research 2.png"),
-          title: "Market Research 2",
-        },
-      ],
-      technologies: ["XD", "Illustrator"],
-      category: "Visual Design",
-      github: "",
-      date: "May, 2020 - July, 2020",
-      visit: "",
-      description:
-        "Bunder is an intuitive web platform for micro-communities to share sensitive data with privacy within the community and allow engagement amongst them. Our aim is two-fold - Co-existing and Collaboration. The former is executed by providing a platform for communities that are geographically positioned in the same proximity (Housing societies, Dorms, Apartment Complexes) to have and share data (exact apartment addresses, phone number, apartment housing rules, individuals tested positive for COVID-19) in an exclusive platform. The latter is to give these people in the community the ability to carry out a task you are physically or remotely unable to do; with dignity and ease.",
     },
   ],
 };
